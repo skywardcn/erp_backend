cfc2e0a850217613a9bf202e45f1430e35f20d1c
skywardcn <hjg16@163.com>
2023-10-26 22:49:33 +0800

    onload

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import App from './App';
 import ReactDOM from 'react-dom';
 
-setTimeout(() => {
+
+window.onload = () => {
     ReactDOM.render(
         <App />,
         document.getElementsByClassName("right-bar")[0]
     )
-}, 3000)
-
+}
