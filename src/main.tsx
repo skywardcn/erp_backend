d1a8520ab13b0fe952e798aed173a1b5c8a858d2
skywardcn <hjg16@163.com>
2023-10-26 10:22:06 +0800

    basic

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,6 @@
-// ==UserScript==
-// @name         My Tampermonkey Script
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
-// @match        https://*/*
-// @grant        none
-// ==/UserScript==
-
-// import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createElement } from 'react';
 
 
-ReactDOM.render(<App />, document.getElementById("basic"));
+ReactDOM.render(<App />,ReactDOM.render(createElement("div"),document.getElementById("basic")))
